859e325defbce40e83a266b5fa527e553e53f173
Cecilio Cannavacciuolo <c.cannavacciuolo@anoki.it>
2021-06-24 22:43:15 +0200

    add evironment variables gitalb

--- src/apis/smktest/model.js
+++ src/apis/smktest/model.js
@@ -9,6 +9,17 @@ const schemaSmktest = mongoose.Schema({
   testId: String,
   testDuration: Number,
   passTest: Boolean,
+  GITLAB_USER_ID: String,
+  CI_PROJECT_URL: String,
+  CI_PROJECT_TITLE: String,
+  CI_PROJECT_NAME: String,
+  CI_PROJECT_ID: String,
+  CI_PIPELINE_ID: String,
+  CI_COMMIT_TAG: String,
+  CI_COMMIT_REF_NAME: String,
+  CI_COMMIT_SHA: String,
+  CI_COMMIT_MESSAGE: String,
+  CI_COMMIT_TITLE: String,
 });
 
 module.exports.Smktest = mongoose.model('smktest', schemaSmktest);
